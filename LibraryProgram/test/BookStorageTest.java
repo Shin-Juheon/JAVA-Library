5266a085b6f8c4335383ff2439457abaf327680b
Shin Juheon <juheon8794@gmail.com>
2025-11-25 21:26:27 +0900

    입력을 위한 테스트 코드 개발 완료

--- LibraryProgram/test/BookStorageTest.java
+++ LibraryProgram/test/BookStorageTest.java
@@ -1,10 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
 class BookStorageTest {
+    BookStorage bookStorage;
+    Book inputBook;
+
+    @BeforeEach
+    void setUp() {
+        bookStorage = new BookStorage();
+        inputBook = new Book("홍길동전","소설", "허균");
+    }
 
     @Test
     void addBook() {
-        //
+        bookStorage.addBook(inputBook);
+
+        assertTrue(bookStorage.getBooks().contains(inputBook));
     }
 }
